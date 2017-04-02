9e9b2ba2aa450c2442afceeca88292c3b2977e35
Mateusz Kasprzak <kacpak@gmail.com>
2017-04-02 17:00:40 +0200

    Vue init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    Hello in Vue, {{ name }}
+  </div>
+</template>
+
+<script lang="typescript">
+  export default {
+    name: 'App',
+    data () {
+      return {
+        name: 'Matt'
+      }
+    }
+  }
+</script>
