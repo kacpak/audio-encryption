8e492b72cd0cf280cb2938bc8c9008474185e319
Mateusz Kasprzak <kacpak@gmail.com>
2017-05-03 09:55:26 +0200

    Add bootstrap@3 for css work

--- src/app/services/cipher.ts
+++ src/app/services/cipher.ts
@@ -1,12 +1,12 @@
 export class Cipher {
   encrypt(message: string, encryptionKey: string): string {
-    const shift = parseFloat(encryptionKey);
-    return this.shift(message, shift);
+    // const shift = parseFloat(encryptionKey);
+    return message;//this.shift(message, shift);
   }
 
   decrypt(message: string, encryptionKey: string): string {
-    const shift = parseFloat(encryptionKey);
-    return this.shift(message, -shift);
+    // const shift = parseFloat(encryptionKey);
+    return message;//this.shift(message, -shift);
   }
 
   shift(message: string, shift: number): string {
