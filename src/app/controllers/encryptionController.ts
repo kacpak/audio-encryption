040d5cb2d6f3879caa9c8f9f0f06100bc3091256
Marek <mar94bor@gmail.com>
2017-06-11 21:21:36 +0200

    Wstępne ukończenie szyfrowania: * Użycie biblioteki zewnętrznej blueimp-md5 do obliczania funkcji skrótu z klucza podanego przez użytkownika * Dodanie pliku TextConversion.ts odpowiedzialnego za zamianę łańcuchów znakowych na ciągi binarne i na odwrót

--- src/app/controllers/encryptionController.ts
+++ src/app/controllers/encryptionController.ts
@@ -1,19 +1,30 @@
 import {DES} from '../des/DES';
 import {KeyGenerator} from '../des/KeyGenerator';
+import {TextConversion} from '../des/TextConversion';
+import md5 = require('blueimp-md5');
 
 export class EncryptionController {
   roundKeys: string[];
 
-  constructor(private encryptedTextArea: HTMLTextAreaElement, private encryptionKeyInput: HTMLInputElement) {
-    this.roundKeys = KeyGenerator.generateKeys(this.encryptionKeyInput.value);
-  }
+  constructor(private encryptedTextArea: HTMLTextAreaElement, private encryptionKeyInput: HTMLInputElement) {}
 
   getOriginalMessage(): string {
-    return DES.decrypt(this.encryptedTextArea.value, this.roundKeys);
+    this.generateRoundKeys();
+    const binaryMessage: string  = TextConversion.convertTextToBinary(this.encryptedTextArea.value);
+    const decryptedMessage: string = DES.decrypt(binaryMessage, this.roundKeys);
+    return TextConversion.convertBinaryToText(decryptedMessage).trim();
   }
 
   setEncryptedText(message: string): void {
-    const encryptedMessage = DES.encrypt(message, this.roundKeys);
-    this.encryptedTextArea.value = encryptedMessage;
+    this.generateRoundKeys();
+    const binaryMessage: string  = TextConversion.convertTextToBinary(message);
+    const encryptedMessage = DES.encrypt(binaryMessage, this.roundKeys);
+    this.encryptedTextArea.value = TextConversion.convertBinaryToText(encryptedMessage);
+  }
+
+  generateRoundKeys(): void {
+    let hash: string = md5(this.encryptionKeyInput.value);
+    let key: string = TextConversion.convertHashToBinaryKey(hash);
+    this.roundKeys = KeyGenerator.generateKeys(key);
   }
 }
